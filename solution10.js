f802a602fb33c075a2e8110ccf56df9c0daee7e0
white-jang <galaxy__12@naver.com>
2021-01-22 21:19:59 +0900

    주석 수정

--- solution10.js
+++ solution10.js
@@ -1,8 +1,15 @@
-var count = prompt();
+const n = prompt('숫자를 입력하세요.');
+let tree = '';
 
-for (var i = 0; i <= count; i++) {
-    for (var j = 1; j <= i+2; j++) {
-        console.log("*");
-    }
-    console.log("\n");
+for(let i=1; i<=n; i++){
+  let star = ''; // 트리 초기화
+  for(let j=1; j<=n-i; j++){
+    star += ' ';    // 왼쪽 정렬용 공백
+  }
+  for(let k=1; k<=2*i-1; k++){
+    star += '*';    // 트리
+  }
+  tree += star + '\n';
 }
+
+console.log(tree);
